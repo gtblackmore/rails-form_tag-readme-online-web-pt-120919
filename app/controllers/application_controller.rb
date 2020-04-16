e6b0d8179c1d4347d4a077f0606ab998ee97e931
George Blackmore <gtblackmore@gmail.com>
2020-04-16 15:56:53 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  config.web_console.whitelisted_ips = '<IP address here>'
   protect_from_forgery with: :exception
 end
